9ad229b6947e9a97142ca4b6f203fd4dedf1668f
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-01-11 20:15:37 -0500

    update header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,20 @@ import logo from '../assets/logo.png';
 function Header() {
     return (
         <header className='flex justify-between items-center font-medium mx-[2vw] my-2 md:my-6'>
-            <div className="group flex items-center text-2xl sm:text-3xl no-select">
-                <img src={logo} alt="Logo" className="w-8 h-8 sm:w-10 sm:h-10" />
-                <h1 className='text-gradient'>Streaks</h1>
-            </div>
+            <Link to='/'>
+                <div className="group flex items-center no-select">
+                    <img src={logo} alt="Logo" className="w-8 h-8 sm:w-10 sm:h-10" />
+                    <h1 className='text-gradient text-2xl sm:text-3xl'>Streaks</h1>
+                </div>
+            </Link>
 
-            <div className="group flex gap-1 justify-center items-center bg-[#192737] mt-1 p-[2px] sm:p-2 pr-1 sm:pr-4 rounded-full">
-                <CiUser className='w-6 h-6 sm:w-8 sm:h-8 border-[1px] rounded-full border-transparent text-custom-text group-hover:text-custom-hover'/>
-                <button className='text-xs sm:text-lg text-custom-text group-hover:text-custom-hover'>Log In</button>
-            </div>
+            <Link to='/create-account'>
+                <div className="group flex gap-1 justify-center items-center bg-[#192737] mt-1 p-2 pr-4 rounded-full">
+                    <CiUser className='w-6 h-6 sm:w-8 sm:h-8 rounded-full'/>
+                    <button className='text-md sm:text-lg text-custom-text group-hover:text-custom-hover'>Log In</button>
+                </div>
+            </Link>
+            
         </header>
     );
 };
