66faa239c3d25713a54ad502848974a69b8c12ff
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-01-12 21:33:41 -0500

    added logging

--- netlify/functions/initialize.js
+++ netlify/functions/initialize.js
@@ -11,7 +11,9 @@ exports.handler = async function(event, context) {
   let visitorData;
 
   try {
+    console.log('Connecting to the database...');
     await client.connect();
+    console.log('Successfully connected to the database.');
 
     const usersCollection = client.db("StreaksDB").collection("users");
     const guestsCollection = client.db("StreaksDB").collection("guests");
@@ -28,7 +30,7 @@ exports.handler = async function(event, context) {
       visitorData = { uuid: newUUID };
     }
   } catch (error) {
-    console.error(error);
+    console.error('An error occurred while connecting to the database:', error);
   } finally {
     await client.close();
   }
