2e3d9ed2ad37eb23ae1eb54bfe10632271d13dc4
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-01-15 15:52:24 -0500

    add firebaseConfig for auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './firebaseConfig';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import EmailVerificationPage from './pages/EmailVerificationPage';
