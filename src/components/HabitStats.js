24fb13f9015232c650a3bae5804c88b20772e39a
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-02-03 01:01:01 -0500

    finalize data initialization

--- src/components/HabitStats.js
+++ src/components/HabitStats.js
@@ -1,73 +1,127 @@
-import React, { useState, useEffect } from 'react';
+// HabitsStats.js
+import React, { useState, useEffect, useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
 import ReactCalendar from 'react-calendar';
 import { db } from '../firebaseConfig';
-import { collection, doc, getDocs, query, orderBy } from "firebase/firestore";
-import './Calendar.css';
+import { collection, doc, getDocs, query, orderBy, addDoc, setDoc, onSnapshot } from "firebase/firestore";import './Calendar.css';
 
-function HabitsStats({ user, guestUUID, habitName }) {
-  const [value, onChange] = useState(new Date());
-  const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState('');
-  const [habitData, setHabitData] = useState({});
+function HabitsStats({ activeHabit }) {
+    const { user, guestUUID } = useContext(UserContext);
+    const [value, onChange] = useState(new Date());
+    const [items, setItems] = useState([]);
+    const [habitData, setHabitData] = useState({});
+    const [currentDate, setCurrentDate] = useState(null);
+    const [notes, setNotes] = useState('');
 
-  const handleAddItem = () => {
-    setItems([...items, newItem]);
-    setNewItem('');
-  };
+    const addDate = async (date, success = false, notes = null) => {
+        if (!activeHabit) return
+        let datesCollection;
+        if (user) {
+            datesCollection = collection(db, 'users', user.uid, 'habits', activeHabit, 'dates');
+        } else if (guestUUID) {
+            datesCollection = collection(db, 'guests', guestUUID, 'habits', activeHabit, 'dates');
+        }
 
-  const tileClassName = ({ date, view }) => {
-    // Add class to dates with habit data
-    if (view === 'month' && habitData[date.toISOString().split('T')[0]]) {
-      return 'habit-day';
-    }
-  };
+        if (datesCollection) {
+            const dateDoc = doc(datesCollection, date.toISOString().split('T')[0]);
+            await setDoc(dateDoc, { date, success, notes }, { merge: true });
+        }
+    };
 
-  // Fetch dates from Firestore
-  const fetchDates = async () => {
-    let datesCollection;
-    if (user) {
-        datesCollection = collection(db, 'users', user.uid, 'habits', habitName, 'dates');
-    } else if (guestUUID) {
-        datesCollection = collection(db, 'guests', guestUUID, 'habits', habitName, 'dates');
-    }
+    const tileClassName = ({ date, view }) => {
+        // Add class to dates with habit data
+        if (view === 'month' && habitData[date.toISOString().split('T')[0]]) {
+            return 'habit-day';
+        }
+    };
 
-    if (datesCollection) {
-        const datesQuery = query(datesCollection, orderBy('date'));
-        const datesSnapshot = await getDocs(datesQuery);
-        const dates = datesSnapshot.docs.map(doc => doc.data());
-        setHabitData(dates.reduce((acc, date) => ({ ...acc, [date.date]: date }), {}));
-    }
-  };
+    // Fetch dates from Firestore
+    const fetchDates = async () => {
+        if (!activeHabit) return;
+        let datesCollection;
+        if (user) {
+            datesCollection = collection(db, 'users', user.uid, 'habits', activeHabit, 'dates');
+        } else if (guestUUID) {
+            datesCollection = collection(db, 'guests', guestUUID, 'habits', activeHabit, 'dates');
+        }
+    
+        if (datesCollection) {
+            const datesQuery = query(datesCollection, orderBy('date'));
+            const datesSnapshot = await getDocs(datesQuery);
+            const dates = datesSnapshot.docs.map(doc => doc.data());
+            setHabitData(dates.reduce((acc, date) => ({ ...acc, [date.date]: date }), {}));
+        }
+    };
 
-  // Fetch dates when component mounts or habitName changes
-  useEffect(() => {
-    fetchDates();
-  }, [user, guestUUID, habitName]);
+    const submitNotes = async () => {
+        if (!activeHabit || !currentDate) return;
+        if (currentDate) {
+            let dateDocRef;
+            if (user) {
+                dateDocRef = doc(db, 'users', user.uid, 'habits', activeHabit, 'dates', currentDate.toISOString().split('T')[0]);
+            } else {
+                dateDocRef = doc(db, 'guests', guestUUID, 'habits', activeHabit, 'dates', currentDate.toISOString().split('T')[0]);
+            }
+            await setDoc(dateDocRef, { date: currentDate, notes: notes }, { merge: true });
+        }
+    };
 
-  return (
-    <div className='flex flex-col w-1/2 h-full max-md:hidden gap-4'>
-      <div className='border-2 rounded-lg'>
-        <ReactCalendar
-          onChange={onChange}
-          value={value}
-          tileClassName={tileClassName}
-          calendarType="gregory"
-        />
-      </div>
-      <div className='border-2 rounded-lg h-full'>
-        <h1 className='font-medium border-b-[1px] mb-4 w-fit max-sm:border-custom-text max-sm:hover:border-custom-hover max-sm:hover:text-custom-hover no-select self-center'>Notes</h1>
-        <div className='flex flex-col w-full justify-center gap-4'>
-          <div>
-            <input className='bg-gray-400 text-black mr-4' value={newItem} onChange={e => setNewItem(e.target.value)} />
-            <button onClick={handleAddItem}>Add Item</button>
-          </div>
-          <ul>
-            {items.map((item, index) => <li key={index}>{item}</li>)}
-          </ul>
+    useEffect(() => {
+        fetchDates();
+    }, [user, guestUUID, activeHabit]);
+    
+    useEffect(() => {
+        if (currentDate) {
+            let dateDocRef;
+            if (user) {
+                dateDocRef = doc(db, 'users', user.uid, 'habits', activeHabit, 'dates', currentDate.toISOString().split('T')[0]);
+            } else {
+                dateDocRef = doc(db, 'guests', guestUUID, 'habits', activeHabit, 'dates', currentDate.toISOString().split('T')[0]);
+            }
+    
+            const unsubscribe = onSnapshot(dateDocRef, (doc) => {
+                if (doc.exists()) {
+                    const data = doc.data();
+                    setNotes(data.notes || '');
+                } else {
+                    // If the document for the new date doesn't exist, clear the notes
+                    setNotes('');
+                }
+            });
+    
+            return unsubscribe;
+        }
+    }, [user, guestUUID, activeHabit, currentDate]);
+
+    console.log('Active Habit: ', activeHabit, 'Current Date: ', currentDate, 'Notes: ', notes);
+    
+    return (
+        <div className='flex flex-col w-1/2 h-full max-md:hidden gap-4'>
+            <div className='border-2 rounded-lg'>
+                <ReactCalendar
+                onChange={date => { onChange(date); setCurrentDate(date); }}
+                value={value}
+                tileClassName={tileClassName}
+                calendarType="gregory"
+            />
+            </div>
+            <button 
+                onClick={() => addDate(currentDate, true, notes)}
+                className='bg-green-600 text-white rounded-lg p-2 hover:bg-green-500'
+            >
+                    Set Success</button>
+            <div className='border-2 rounded-lg h-full'>
+                <h1 className='font-medium border-b-[1px] mb-4 w-fit max-sm:border-custom-text max-sm:hover:border-custom-hover max-sm:hover:text-custom-hover no-select self-center'>Notes</h1>
+                <div className='flex flex-col w-full justify-center gap-4'>
+                    <div>
+                        <input type="text" value={notes} onChange={e => setNotes(e.target.value)} />
+                        <button onClick={submitNotes}>Submit Notes</button>
+                    </div>
+                    <p>{notes}</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default HabitsStats;
