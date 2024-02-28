854562f2df1828b0c65c5794e62f5e5a749e51eb
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-02-27 22:54:06 -0500

    update mainPage layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import Wave from '../assets/wave.svg';
 function Layout({ children }) {
   return (
     <div className="bg-[#13151b] flex flex-col justify-between min-w-screen min-h-screen relative">
-        <img src={Wave} className="absolute w-full h-full bottom-0 object-cover" alt="blob" />
+        <img src={Wave} className="absolute w-full h-full bottom-0 object-cover" alt="wave" />
         <div className="relative z-10 flex-grow flex items-center justify-center">{children}</div>
     </div>
   );
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -8,7 +8,7 @@ function MainPage() {
     const [activeHabit, setActiveHabit] = useState(null);
 
     return (
-        <div className='flex flex-col w-full h-full'>
+        <div className='flex flex-col justify-between w-full min-h-screen'> {/* Change h-screen to min-h-screen */}
             <Header />
             <div className='card text-custom-text max-w-screen-xl self-center lg:p-8 m-4 bg-custom-light bg-opacity-85 backdrop-blur-md rounded-lg shadow-lg'>
                 <div className='flex justify-center p-6 text-center gap-6 text-3xl'>
@@ -16,6 +16,7 @@ function MainPage() {
                     {activeHabit && <HabitStats activeHabit={activeHabit} />}
                 </div>
             </div>
+            <footer className='h-24'></footer>
         </div>
     );
 };
